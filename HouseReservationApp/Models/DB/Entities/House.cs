f4687043e209deb5b99218b9a0bd370e3029b381
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-05 21:29:02 +0100

    feat(entity): Add attributes for house and reservation entities

--- HouseReservationApp/Models/DB/Entities/House.cs
+++ HouseReservationApp/Models/DB/Entities/House.cs
@@ -1,23 +1,64 @@
 ﻿namespace HouseReservationApp.Models.DB.Entities
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class House
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int OwnerId { get; set; }
+
+        [Required]
+        [StringLength(200)]
         public string Title { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal PricePerNight { get; set; }
+
+        [Required]
         public int SizeM2 { get; set; }
+
+        [Required]
         public int Bedrooms { get; set; }
+
+        [Required]
         public int Bathrooms { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Country { get; set; }
+
+        [StringLength(100)]
         public string? State { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string StreetName { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string StreetNumber { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string PostalCode { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
 
+        [ForeignKey("OwnerId")]
         public User Owner { get; set; }
-        public ICollection<Reservation> Reservations { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
     }
+
 }
--- HouseReservationApp/Models/DB/Entities/Reservation.cs
+++ HouseReservationApp/Models/DB/Entities/Reservation.cs
@@ -1,18 +1,47 @@
-﻿namespace HouseReservationApp.Models.DB.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HouseReservationApp.Models.DB.Entities
 {
     public class Reservation
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int HouseId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
         public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(20)]
         public string Phone { get; set; }
+
+        [Required]
         public DateTime CheckInDate { get; set; }
+
+        [Required]
         public DateTime CheckOutDate { get; set; }
+
+        [Required]
         public ReservationStatus Status { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
 
+        [ForeignKey("HouseId")]
         public House House { get; set; }
     }
 }
