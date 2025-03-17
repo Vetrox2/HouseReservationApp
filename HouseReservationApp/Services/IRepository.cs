e4ab83f698e0333df0ca75075a982f19232eef07
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-17 20:05:17 +0100

    feat(interface): add IRepository

---
+++ HouseReservationApp/Services/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseReservationApp.Services
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll();
+        Task<T?> Get(int id);
+        Task Update(T item);
+        Task<bool> Delete(int id);
+    }
+}
