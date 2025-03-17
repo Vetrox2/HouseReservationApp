8f02bd43fd376d61da32ca7c7201d3aa4f7645ee
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-17 20:05:50 +0100

    feat(class): add Repository (not fully implemented)

---
+++ HouseReservationApp/Services/Repository.cs
@@ -0,0 +1,37 @@
+﻿
+using HouseReservationApp.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseReservationApp.Services
+{
+    public class Repository<T>(HouseReservationContext context) : IRepository<T> where T : class
+    {
+        private readonly HouseReservationContext _context = context;
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public async Task<bool> Delete(int id)
+        {
+            var item = await _dbSet.FindAsync(id);
+            if (item is null) return false;
+
+            _dbSet.Remove(item);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<T?> Get(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IQueryable<T>> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public async Task Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
