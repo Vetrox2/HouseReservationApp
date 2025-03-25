3ed0c7e941067507413ea23286ad09aa78bdf074
Vetrox2 <mateuszmulka@spoko.pl>
2025-03-26 00:14:37 +0100

    feat(model): add PagedResult model for paged data

---
+++ HouseReservationApp/Models/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace HouseReservationApp.Models
+{
+    public record PagedResult<T>(
+    IEnumerable<T> Items,
+    int TotalCount,
+    int CurrentPage,
+    int PageSize)
+    {
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
