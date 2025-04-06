cfe49551bc319866e735c22bd746743cc00240e5
Vetrox2 <mateuszmulka@spoko.pl>
2025-04-06 15:49:39 +0200

    feat(models): add UserIndexViewModel

---
+++ Contracts/Models/ViewModels/UserIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HouseReservation.Contracts.Models.ViewModels
+{
+    public class UserIndexViewModel
+    {
+        public PagedResult<UserDto> PagedResult { get; set; }
+        public string? FirstNameFilter { get; set; }
+        public string? LastNameFilter { get; set; }
+        public string? SortBy { get; set; }
+        public SortDirection? SortDirection { get; set; }
+        public string? SortDirectionIcon => SortDirection is not null ? SortDirection == Models.SortDirection.Ascending ? "↑" : "↓" : null;
+    }
+}
