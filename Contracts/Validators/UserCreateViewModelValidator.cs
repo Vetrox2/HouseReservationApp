9111cb60940d4800c3eae8afdd9ddf8007dc38df
Vetrox2 <mateuszmulka@spoko.pl>
2025-04-09 19:20:12 +0200

    feat(UserCreateViewModel): extract validation to validator

---
+++ Contracts/Validators/UserCreateViewModelValidator.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using HouseReservation.Contracts.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseReservation.Contracts.Validators
+{
+    public class UserCreateViewModelValidator : AbstractValidator<UserCreateViewModel>
+    {
+        //dodaj validacje dla email z bazy i email
+        public UserCreateViewModelValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(100).WithMessage("First name cannot exceed 100 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(100).WithMessage("Last name cannot exceed 100 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email address.")
+                .MaximumLength(255).WithMessage("Email cannot exceed 255 characters.");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .MaximumLength(20).WithMessage("Phone number cannot exceed 20 characters.")
+                .Matches(@"^\+?[0-9\s\-()]+$").WithMessage("Invalid phone number format.");
+
+            RuleFor(x => x.BankAccount)
+                .NotEmpty().WithMessage("Bank account is required.")
+                .MaximumLength(50).WithMessage("Bank account cannot exceed 50 characters.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.");
+
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty().WithMessage("Please confirm your password.")
+                .Equal(x => x.Password).WithMessage("The password and confirmation password do not match.");
+        }
+    }
+}
